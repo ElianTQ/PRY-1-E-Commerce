6e51d6d5c0089cc0c47d5c5ecde22d813cae48f4
Elian Trejos <e.trejos.1@estudiantec.cr>
2026-09-07 02:52:07 -0600

    inicializando proyecto react + vite en rama dev

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Proyecto 1 - E-commerce B2C</h1>
+      <p>Aplicación en desarrollo...</p>
+    </div>
+  )
+}
+
+export default App
