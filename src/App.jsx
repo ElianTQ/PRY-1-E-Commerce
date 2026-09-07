6887b7ece812592ee958e8db4c3f32e463c38a84
D31N3R <deienerc3@gmail.com>
2026-09-07 14:16:31 -0600

    Setup: configuración inicial, corrección

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
-{/*import CatalogPage from "./features/catalog/CatalogPage";
-import ProductDetail from "./pages/ProductDetail";*/}
+import CatalogPage from "./features/catalog/CatalogPage";
+import ProductDetail from "./pages/ProductDetail";
 
 export default function App() {
   return (
     <HashRouter>
       <Routes>
-        {/*<Route path="/" element={<CatalogPage />} />
-        <Route path="/producto/:id" element={<ProductDetail />} />*/}
+        <Route path="/" element={<CatalogPage />} />
+        <Route path="/producto/:id" element={<ProductDetail />} />
       </Routes>
     </HashRouter>
   );
---
+++ src/features/catalog/CatalogPage.jsx
@@ -0,0 +1,4 @@
+// src/features/catalog/CatalogPage.jsx
+export default function CatalogPage() {
+  return <div>Catálogo (en construcción)</div>;
+}
